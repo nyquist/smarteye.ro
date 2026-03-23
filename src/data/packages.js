bceecfba5f6549d39e50e499d18d6e7272372202
Nyquist <nyquist@vaspun.eu>
2026-03-23 22:32:27 +0100

    Update Astro configuration to set site URL and output format

---
+++ src/data/packages.js
@@ -0,0 +1,54 @@
+export const packagesRo = [
+	{
+		slug: 'sistem-alarmare-cu-fir',
+		name: 'Sistem de Alarmare cu Fir',
+		price: '179 EUR fara TVA',
+		items: [
+			'Centrala Paradox SP4000',
+			'1 senzor de miscare',
+			'1 senzor magnetic',
+			'Tastatura cu fir',
+			'Sirena interioara',
+			'Instalare gratuita'
+		]
+	},
+	{
+		slug: 'sistem-alarmare-wireless',
+		name: 'Sistem de Alarmare Wireless',
+		price: '289 EUR fara TVA',
+		items: [
+			'Centrala Paradox MG5000',
+			'1 senzor miscare fara fir',
+			'1 senzor magnetic fara fir',
+			'Tastatura cu fir',
+			'Sirena interioara',
+			'Instalare gratuita'
+		]
+	},
+	{
+		slug: 'sistem-supraveghere-video-interior',
+		name: 'Sistem Supraveghere Video Interior',
+		price: '349 EUR fara TVA',
+		items: [
+			'DVR standalone, 4 intrari',
+			'HDD 500 GB',
+			'2 camere interior',
+			'2 alimentatoare',
+			'Acces internet',
+			'Instalare gratuita'
+		]
+	},
+	{
+		slug: 'sistem-supraveghere-video-exterior',
+		name: 'Sistem Supraveghere Video Exterior',
+		price: '399 EUR fara TVA',
+		items: [
+			'DVR standalone, 4 intrari',
+			'HDD 500 GB',
+			'2 camere exterior',
+			'2 alimentatoare',
+			'Acces internet',
+			'Instalare gratuita'
+		]
+	}
+];
